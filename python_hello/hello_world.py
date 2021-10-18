a8f0832b882a1cd1c1bb7362c1c9e20265dccd75
William Zhao <William@sunpan.local>
2021-10-18 17:11:49 -0400

    python_hello #1

---
+++ python_hello/hello_world.py
@@ -0,0 +1,7 @@
+from python_world import world
+
+def hello_world():
+    return "hello, " + world.world()
+
+if __name__ == '__main__':
+    print(hello_world())
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'python_hello',
+    version = '0.0.1',
+    author = 'wwzz3077',
+    author_email = 'wtzhao@hotmail.com',
+    description = 'A function that returns [hello, world]',
+    long_description = long_description,
+    url = 'https://github.com/wwzz3077/python_hello',
+    packages= setuptools.find_packages(),
+    keywords= ['python', 'wwzz3077'],
+    install_requires = [
+        'python_world@https://github.com/wwzz3077/python_world#egg=python_world'
+    ]
+)
